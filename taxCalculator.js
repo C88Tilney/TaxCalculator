52b0f42fc45511e269204e7b2d4c30d61bcfa554
C88Tilney <CT@C-MacBook.local>
2016-07-22 15:30:45 +0100

    JS change CSS new look
    JS i updated code to fit the new tax bands that are correct for this
    year. Also round figures out to 2decimal places.
    Css response better
    HTML small correction to bands

--- taxCalculator.js
+++ taxCalculator.js
@@ -15,30 +15,54 @@ document.getElementById("calculate").addEventListener("click", function tax() {
         money = "You pay no tax";
     }
 
-    else if (x >= 11000 && x < 43000) {
+    else if (x >= 11000 && x < 32000) {
         money = ((x - 11000) * 0.20);
     }
 
-    else if (x > 43001 && x < 122000) {
-        money = ((x - 11000) * 0.40);
+    else if (x > 32001 && x < 99000) {
+        var totalTaxable = (x - 11000);
+        console.log(totalTaxable);
+        var moneyTaxedAtHigherRate = (x - (32000+11000));
+        console.log(moneyTaxedAtHigherRate);
+        var basicRateAmount = ((totalTaxable - moneyTaxedAtHigherRate) * 0.20);
+        console.log(basicRateAmount);
+        var higerRateAmount = (moneyTaxedAtHigherRate * .40);
+        console.log(higerRateAmount);
+        money = (basicRateAmount + higerRateAmount);
     }
 
-    else if (x > 122001 && x < 150000) {
-        money = (x * 0.40);
+    else if (x > 100000 && x < 149999) {
+        console.log(x);
+        var moneyTaxedAtHigherRate = (x - 32000);
+        console.log(moneyTaxedAtHigherRate);
+        var basicRateAmount = (32000 * 0.20);
+        console.log(basicRateAmount);
+        var higerRateAmount = (moneyTaxedAtHigherRate * .40);
+        console.log(higerRateAmount);
+        money = (basicRateAmount + higerRateAmount);
+        
     }
     else if (x >= 150000) {
-        money = (x * 0.45);
+        console.log(x);
+        var moneyTaxedAtHigherRate = (150000 - 32000);
+        console.log(moneyTaxedAtHigherRate);
+        var basicRateAmount = (32000 * 0.20);
+        console.log(basicRateAmount);
+        var higerRateAmount = (moneyTaxedAtHigherRate * .40);
+        console.log(higerRateAmount);
+        var additionalRateBand = ((x - 150000)* .45)
+        money = (basicRateAmount + higerRateAmount + additionalRateBand);
     }
     
     console.log(money);
-
-    document.getElementById("yearlyTax").innerHTML = money;
-    
     var monthlyTax = (money/12);
     var weeklyTax = (money/52);
+    var takeHome = (x-money);
     
-    document.getElementById("monthlyTax").innerHTML = monthlyTax;
-    document.getElementById("weeklyTax").innerHTML = weeklyTax;
     
+    document.getElementById("yearlyTax").innerHTML = money.toFixed(2);
+    document.getElementById("monthlyTax").innerHTML = monthlyTax.toFixed(2);
+    document.getElementById("weeklyTax").innerHTML = weeklyTax.toFixed(2);
+    document.getElementById("takeHome").innerHTML = takeHome.toFixed(2);
 
 });
